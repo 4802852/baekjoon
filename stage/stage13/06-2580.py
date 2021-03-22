c816ac4618f96fc66d004d8f28376f95ae0cfa26
4802852 <4802852@gmail.com>
2021-03-23 01:29:01 +0900

    stage13 problem6 clear

---
+++ stage/stage13/06-2580.py
@@ -0,0 +1,48 @@
+import sys
+
+
+sudoku = []
+for i in range(9):
+    a = list(map(int, sys.stdin.readline().split()))
+    sudoku.append(a)
+zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
+
+
+def check(i, j):
+    pool = [i for i in range(1, 10)]
+    for k in range(9):
+        if sudoku[i][k] in pool:
+            pool.remove(sudoku[i][k])
+        if sudoku[k][j] in pool:
+            pool.remove(sudoku[k][j])
+    i //= 3
+    j //= 3
+    for p in range(i * 3, (i + 1) * 3):
+        for q in range(j * 3, (j + 1) * 3):
+            if sudoku[p][q] in pool:
+                pool.remove(sudoku[p][q])
+    return pool
+
+
+ans = False
+
+
+def solve_sudoku(x):
+    global ans
+    if ans:
+        return
+    if x == len(zeros):
+        for row in sudoku:
+            print(*row)
+        ans = True
+        return
+    else:
+        (i, j) = zeros[x]
+        possible = check(i, j)
+        for num in possible:
+            sudoku[i][j] = num
+            solve_sudoku(x + 1)
+            sudoku[i][j] = 0
+
+
+solve_sudoku(0)
