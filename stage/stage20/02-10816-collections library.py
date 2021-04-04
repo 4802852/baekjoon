e6392ac19452e59a0e670b747e25543883548223
hongseok choi <4802852@gmail.com>
2021-04-04 18:24:03 +0900

    stage19 problem2 clear

--- stage/stage20/02-10816-collections library.py
+++ stage/stage20/02-10816-collections library.py
@@ -5,7 +5,6 @@ lst = sorted(list(map(int, input().split()))) # 정렬
 M = int(input())
 card = list(map(int, input().split()))
 
-
 counter = Counter(lst)
 
 print(' '.join(str(counter[c]) if c in counter else '0' for c in card))
