02233ba07b99975702a99e412a6269becc23abbd
hongseok choi <4802852@gmail.com>
2021-04-18 09:20:47 +0900

    stage25 problem3 clear

--- stage/stage21-30/stage25/03-9370.py
+++ stage/stage21-30/stage25/03-9370.py
@@ -2,7 +2,7 @@ import sys
 import heapq
 
 
-def Dijkstara(start, end):
+def Dijkstara(start):
     visited = [float('inf') for _ in range(v + 1)]
     visited[start] = 0
     heap = []
@@ -13,21 +13,31 @@ def Dijkstara(start, end):
             continue
         for w, n in lines[now]:
             nw = w + weight
-            if nw < visited[n] and nw < visited[end]:
+            if nw < visited[n]:
                 visited[n] = nw
                 heapq.heappush(heap, (nw, n))
-    return visited[end]
-
+    return visited
 
 
 for _ in range(int(sys.stdin.readline())):
-    n, m, t = map(int, sys.stdin.readline().split())
+    v, m, t = map(int, sys.stdin.readline().split())
     s, g, h = map(int, sys.stdin.readline().split())
-    lines = [[] for __ in range(n + 1)]
+    lines = [[] for __ in range(v + 1)]
     for __ in range(m):
         a, b, d = map(int, sys.stdin.readline().split())
-        lines[a].append([d, b])
-        lines[b].append([d, a])
+        if (a == g and b == h) or (a == h and b == g):
+            lines[a].append([d - 0.1, b])
+            lines[b].append([d - 0.1, a])
+        else:
+            lines[a].append([d, b])
+            lines[b].append([d, a])
     target = []
     for i in range(t):
         target.append(int(sys.stdin.readline()))
+    ans = []
+    sr = Dijkstara(s)
+    for tar in target:
+        if sr[tar] != float('inf') and type(sr[tar]) == float:
+            ans.append(tar)
+    ans.sort()
+    print(*ans)
