6819899e7d5def79645ff92f1593b8441c7bdd14
4802852 <4802852@gmail.com>
2021-04-21 22:54:12 +0900

    stage25 problem6 clear

--- stage/stage21-30/stage25/05-11404.py
+++ stage/stage21-30/stage25/05-11404.py
@@ -1,6 +1,25 @@
 import sys
 
+INF = float('inf')
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
-lines = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+s = [[INF] * (n + 1) for i in range(n + 1)]
 
+for i in range(m):
+    a, b, c = map(int, sys.stdin.readline().split())
+    if s[a][b] > c:
+        s[a][b] = c
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i != j and s[i][j] > s[i][k] + s[k][j]:
+                s[i][j] = s[i][k] + s[k][j]
+
+for i in s[1:]:
+    for j in i[1:]:
+        if j == INF:
+            print(0, end=' ')
+        else:
+            print(j, end=' ')
+    print()
