efdeacb0505a8c115e19aac277d252cae7561384
4802852 <4802852@gmail.com>
2021-05-03 01:16:26 +0900

    stage30 problem5 clear

--- stage/stage21-30/stage30/05-2887.py
+++ stage/stage21-30/stage30/05-2887.py
@@ -2,7 +2,7 @@ import sys
 
 
 def find(x):
-    if parent[x] < 0:
+    if parent[x] == x:
         return x
     p = find(parent[x])
     parent[x] = p
@@ -10,37 +10,31 @@ def find(x):
 
 
 def union(x, y):
-    if parent[x] < parent[y]:
-        parent[x] += parent[y]
-        parent[y] = x
-    else:
-        parent[y] += parent[x]
-        parent[x] = y
+    parent[y] = x
 
 
 n = int(sys.stdin.readline())
-parent = [-1 for _ in range(n + 1)]
+parent = [i for i in range(n + 1)]
 lines = []
 stars = []
-for i in range(n):
+for i in range(1, n + 1):
     a, b, c = map(int, sys.stdin.readline().split())
     stars.append([a, b, c, i])
 for j in range(3):
     stars.sort(key=lambda x: x[j])
-    before = stars[0][3]
     for i in range(1, n):
-        current = stars[i][3]
-        lines.append([abs(stars[i][j] - stars[i - 1][j]), before, current])
-        before = current
-lines.sort(key=lambda x: x[0])
-cnt = n - 1
+        lines.append((abs(stars[i - 1][j] - stars[i][j]), stars[i - 1][3], stars[i][3]))
+lines.sort()
+cnt = 0
 ans = 0
-while cnt:
-    d, a, b = lines.pop(0)
+for co in lines:
+    d, a, b = co
     ar = find(a)
     br = find(b)
     if ar != br:
         union(ar, br)
         ans += d
-        cnt -= 1
+        cnt += 1
+    if cnt == n - 1:
+        break
 print(ans)
