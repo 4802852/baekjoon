89e9a53f16e236dbeb98b1b87b13afbc7f9f293e
4802852 <4802852@gmail.com>
2022-04-14 21:55:11 +0900

    ss sw test 19236 complete

--- samsung sw test/19236.py
+++ samsung sw test/19236.py
@@ -12,7 +12,7 @@ from copy import deepcopy
 rc = [-1, -1, 0, 1, 1, 1, 0, -1]
 cc = [0, -1, -1, -1, 0, 1, 1, 1]
 bowl = []
-fish = {}
+fish = {}  # k: 물고기 번호, v: [r, c, direction]
 for i in range(4):
     tmp = list(map(int, input().split()))
     tmp2 = []
@@ -28,8 +28,10 @@ ans = []
 
 
 def move(bowl_arr, fish_dict):
+    # 물고기의 이동
     for n in range(1, 17):
         if n not in fish_dict.keys():
+            # 물고기가 딕셔너리에 없을 경우(상어에게 먹혔을 경우), continue
             continue
         r, c, d = fish_dict[n]
         nr, nc = r + rc[d], c + cc[d]
@@ -40,6 +42,7 @@ def move(bowl_arr, fish_dict):
             or 4 <= nc
             or (nr == fish_dict[-1][0] and nc == fish_dict[-1][1])
         ):
+            # bowl[nr][nc] 가 어항 밖으로 이탈하거나, 상어의 위치일 경우 방향을 반시계 방향으로 전환한다. 이동이 가능할때까지.
             d = (d + 1) % 8
             nr, nc = r + rc[d], c + cc[d]
         fish_to_change = bowl_arr[nr][nc]
@@ -54,7 +57,9 @@ def move(bowl_arr, fish_dict):
 
 
 def shark_move_dfs(bowl_arr, fish_dict, prev_number):
+    # DFS 함수
     ans.append(prev_number)
+    # 물고기의 이동
     move(bowl_arr, fish_dict)
     sr, sc, sd = fish_dict[-1]
     for i in range(1, 4):
@@ -65,6 +70,7 @@ def shark_move_dfs(bowl_arr, fish_dict, prev_number):
             continue
         if n_bowl_arr[nsr][nsc] == 0:
             continue
+        # 상어가 이동하는 위치에 있는 물고기를 먹고(fish 딕셔너리에서 삭제), 해당 위치로 상어가 이동한다.
         to_eat = n_bowl_arr[nsr][nsc]
         nsr, nsc, nsd = n_fish_dict.pop(to_eat)
         n_bowl_arr[nsr][nsc] = 0
